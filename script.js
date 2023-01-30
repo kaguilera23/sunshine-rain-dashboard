0ed53765d1638f9d0a9b175f19c525c846187977
unknown <aguilera.k02@gmail.com>
2023-01-30 09:28:33 -0500

    added variable for next 5 days dates

--- script.js
+++ script.js
@@ -9,14 +9,24 @@ var tempEl = document.getElementById("temperature")
 var humidityEl = document.getElementById("humidity")
 var windSpeedEl = document.getElementById("wind-speed")
 var currentCondition = document.getElementById("condition")
+var submittedSection = document.getElementById("submitted")
+var dayOne = (today.add(1, 'day')).format("YYYY-MM-DD 12:00:00")
+var dayTwo = (today.add(2, 'day')).format("YYYY-MM-DD 12:00:00")
+var dayThree = (today.add(3, 'day')).format("YYYY-MM-DD 12:00:00")
+var dayFour = (today.add(4, 'day')).format("YYYY-MM-DD 12:00:00")
+var dayFive = (today.add(5, 'day')).format("YYYY-MM-DD 12:00:00")
 
 todayEl.textContent = today.format("dddd, M/D/YYYY")
 
-
 searchSection.addEventListener("submit", function (event) {
     event.preventDefault();
+    submittedSection.textContent = "Working on Your Forecast!"
+
+    setTimeout(() => {
+        submittedSection.textContent = ""
+    }, 4000);
+
     var cityInput = (searchBox.value.split(" ").join(""))
-    console.log(cityInput)
     getLatLon(cityInput)
 })
 
@@ -30,10 +40,11 @@ fetch(geocoderUrl)
 }
 )
 .then(function (data) {
-    console.log(data[0])
     var cityLatitude = data[0].lat;
     var cityLongitude = data[0].lon
     getCurrentWeather(cityLatitude, cityLongitude)
+    getFiveDayForecast(cityLatitude, cityLongitude)
+
 });
 }
 
@@ -54,7 +65,18 @@ fetch(currentWeatherUrl)
     currentCondition.textContent = data.weather[0].description
 
     console.log(data.weather[0].icon)
-    console.log(data)
 })
-
 }
+
+function getFiveDayForecast (lat, lon) {
+
+    var fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat +"&lon=" + lon + "&appid=" + apiKey + "&units=imperial"
+    
+    fetch(fiveDayUrl)
+    .then(function(res) {
+        return res.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+    }
